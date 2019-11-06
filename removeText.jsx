339582d93ede37bf28499db9142e8e5e4a289e4b
Loriage <32834445+Loriage@users.noreply.github.com>
2019-11-06 19:15:24 +0100

    Fixing layers parsing

--- removeText.jsx
+++ removeText.jsx
@@ -1,11 +1,18 @@
+findLayersText(app.activeDocument.artLayers);
+browseLayerSets(app.activeDocument.layerSets);
+
 function findLayersText (layers) {
     for (var i = 0, len = layers.length; i < len; i++) {
         if (layers[i].kind == "LayerKind.TEXT") {
             layers[i].remove();
+			i--;
             len--;
         }
     }
 }
+
+//check all art layers then
+//check if there are any inner layer sets
 function browseLayerSets (sets) {
     for (var i = 0, len = sets.length; i < len; i++) {
         findLayersText(sets[i].artLayers);
@@ -15,7 +22,3 @@ function browseLayerSets (sets) {
         }
     }
 }
-findLayersText(app.activeDocument.artLayers);
-browseLayerSets(app.activeDocument.layerSets);
-findLayersText(app.activeDocument.artLayers);
-browseLayerSets(app.activeDocument.layerSets);
