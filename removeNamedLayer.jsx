3d3d5b8a9fd4aaa300adb1758bf5181c1444a533
Loriage <32834445+Loriage@users.noreply.github.com>
2020-10-26 15:09:42 +0100

    Update removeNamedLayer.jsx
    Fix indentation

--- removeNamedLayer.jsx
+++ removeNamedLayer.jsx
@@ -4,9 +4,9 @@ browseLayerSets(app.activeDocument.layerSets);
 function removeNamedLayer (layers) {
 	for (var i = 0, len = layers.length; i < len; i++) {
 	 	if (layers[i].name === "대사 복사") { // replace 대사 복사 by the name of the layer you want to delete.
-	    	layers[i].remove();
-	    	i--;
-	    	len--;
+	    		layers[i].remove();
+	    		i--;
+	    		len--;
 		}
 	}
 }
