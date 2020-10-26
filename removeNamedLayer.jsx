82ea8d3b88c94a5f6e22156bc7d454fed30902e0
Loriage <32834445+Loriage@users.noreply.github.com>
2020-10-26 15:06:49 +0100

    Update removeNamedLayer.jsx
    Adding folder exploration

--- removeNamedLayer.jsx
+++ removeNamedLayer.jsx
@@ -1,7 +1,21 @@
-var layers = app.activeDocument.layers;
+removeNamedLayer(app.activeDocument.artLayers);
+browseLayerSets(app.activeDocument.layerSets);
 
-for (var i = 0; i<layers.length; i++) {
-  if (layers[i].name === "대사 복사") {
-    layers[i].remove;
-  }
+function removeNamedLayer (layers) {
+	for (var i = 0, len = layers.length; i < len; i++) {
+	 	if (layers[i].name === "대사 복사") {
+	    	layers[i].remove();
+	    	i--;
+	    	len--;
+		}
+	}
+}
+function browseLayerSets (sets) {
+    for (var i = 0, len = sets.length; i < len; i++) {
+        removeNamedLayer(sets[i].artLayers);
+
+        if(sets[i].layerSets.length > 0){
+            browseLayerSets(sets[i].layerSets);
+        }
+    }
 }
